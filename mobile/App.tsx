30f42d3715eeabeba56501ab9614c8d27750ecd7
Ryan Max <ryanshift22@gmail.com>
2023-01-31 16:55:47 -0300

    Projeto finalizado

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,18 @@
 import './src/lib/dayjs';
 
 import { StatusBar } from 'react-native';
-import { 
+import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
+import { useEffect } from 'react';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +22,31 @@ export default function App() {
     Inter_800ExtraBold
   });
 
+  async function schedulePushNotification() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync();
+    console.log("Agendadas: ", schedule);
+
+    if (schedule.length > 0) {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+    }
+
+    const trigger = new Date(Date.now());
+    trigger.setHours(trigger.getHours() + 5);
+    trigger.setSeconds(0);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Rodrigo! 😀",
+        body: "Você praticou seus hábitos hoje?"
+      },
+      trigger
+    });
+  }
+
+  useEffect(() => {
+    schedulePushNotification();
+  }, []);
+
   if (!fontsLoaded) {
     return (
       <Loading />
@@ -28,7 +55,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
